74e559acbeaf3aa331d23b9c1753cc9e9460e5e0
Xun Ruan <galaxymacos@gmail.com>
2021-07-03 18:56:09 +0800

    Use stacks to arrange view

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 20){
+            
+            HStack{
+                Text("1 2 3")
+            }
+            HStack{
+                Text("4 5 6")
+            }
+            HStack{
+                Text("7 8 9")
+            }
+            
+            
+            // Stack things together
+            ZStack(alignment: .top){    // alignment is used when you have two different-size thing in stack
+                Text("hey")
+                Text("man")
+            }
+            // Push away items
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView()Spacer()
     }
 }
