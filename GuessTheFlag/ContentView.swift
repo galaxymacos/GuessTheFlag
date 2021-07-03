de517f80a5bdbf22058fa01f4c286d23bbe9ce79
Xun Ruan <galaxymacos@gmail.com>
2021-07-03 20:02:53 +0800

    Gradients

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,30 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            // Make only the text go red
-//            ZStack{
-//                Text("My Content")
-//            }.background(Color.red)
-            
-//            ZStack{
-//                Text("My Content").background(Color.red)
-//            }
-            
-            // Make background go red
-            ZStack{
-                // Turn the whole screen to red
-//                Color.red
-                Color.red.frame(width: 200, height: 200)
-                // make the screen black or white
-//                Color.primary
-                // make the screen less black or less white
-                // make the color cover the safe area
-                Color.secondary.edgesIgnoringSafeArea(.all)
-                Text("My Content")
-            }
-        }
+//        RadialGradient(colors: [.white, .black], center: .center, startRadius: 0, endRadius: 200)
         
+//        LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom).ignoresSafeArea(edges: .all)
+        
+        AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
     }
 }
 
